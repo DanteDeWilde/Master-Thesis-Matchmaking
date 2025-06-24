6e02526ce7559f6361b87bc9ac7cb47eb2bb8efe
Jenno Verdonck <jenno.verdonck@gmail.com>
2025-06-24 15:01:17 +0200

    added a script to merge all downloaded files

---
+++ merge_data.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+folders = ['data_america', 'data_asia', 'data_europe', 'data_sea']
+merged_tables = []
+
+if __name__ == '__main__':
+    for folder in folders:
+        # merge the 3 tables with each other into one table
+        df_matches = pd.read_csv(f'{folder}/matches.csv',sep=';',decimal='.', dtype=str)
+        df_player_match_info = pd.read_csv(f'{folder}/player_match_info.csv',sep=';',decimal='.',dtype=str)
+        df_team_match_info = pd.read_csv(f'{folder}/team_match_info.csv', sep=';', decimal='.',dtype=str)
+
+        merged = pd.merge(df_player_match_info, df_matches, how='left', on='match_UUID')
+        merged = pd.merge(merged, df_team_match_info, how='left', on=['match_UUID', 'teamId'])
+
+        merged_tables.append(merged)
+
+    pd.concat(merged_tables).to_excel(f'merged.xlsx', index=False)
